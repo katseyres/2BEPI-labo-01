5d0d6089dc2ebb2f83661815f1d9525f9f24285a
Maximilien Denis <denismax98@hotmail.fr>
2021-02-14 15:58:26 +0100

    #

--- test_utils.py
+++ test_utils.py
@@ -23,7 +23,7 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(utils.integral("2 * x**2 - 4 * x + 2", 2, -4), -84)
 
 if __name__ == '__main__':
-    with open('results.xml', 'wb') as output:
+    with open('results.xml', 'w') as output:
         unittest.main(
             testRunner=xmlrunner.XMLTestRunner(output=output),
             failfast=False, buffer=False, catchbreak=False)
